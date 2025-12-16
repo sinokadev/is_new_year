215d03586d34fff94acfad2b946c95568a16b9c4
sinokadev <sinoka@sinoka.dev>
2025-12-17 02:37:59 +0900

    teststs.py add detail log

--- teststs.py
+++ teststs.py
@@ -1,11 +1,11 @@
 def run_test(tests, func):
-    for test in tests:
-        result = func(test[0])
-        if result != test[1]:
-            return False, test[0], result
-    return True, "", ""
+    for value, expected in tests:
+        result = func(value)
+        if result != expected:
+            return False, value, expected, result
+    return True, None, None, None
 
 def testtest(tests, func):
     result = run_test(tests, func)
-    if not result[0]: print("Your code is shit", result[1], result[2])
+    if not result[0]: print("Your code is shit", "input:", result[1], "expected:", result[2], "actual", result[3])
     else: print("OK!")
