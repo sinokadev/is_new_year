b51dd611cd56d9e236649bc28202f7ab8d38733d
sinokadev <sinoka@sinoka.dev>
2025-12-17 02:38:45 +0900

    teststs.py add detail option

--- teststs.py
+++ teststs.py
@@ -5,7 +5,10 @@ def run_test(tests, func):
             return False, value, expected, result
     return True, None, None, None
 
-def testtest(tests, func):
+def testtest(tests, func, detail = False):
     result = run_test(tests, func)
-    if not result[0]: print("Your code is shit", "input:", result[1], "expected:", result[2], "actual", result[3])
+    if detail:
+        if not result[0]: print("Your code is shit", "input:", result[1], "expected:", result[2], "actual", result[3])
+    else:
+        if not result[0]: print("Your code is shit", result[1], result[3])
     else: print("OK!")
